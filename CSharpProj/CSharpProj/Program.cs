52612433176ff67a1dfe2674eda2b03e5ff042d1
leontmp <leontmaria@yandex.ru>
2017-08-12 21:20:27 +0300

    CSharp-hello-feature

---
+++ CSharpProj/CSharpProj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
